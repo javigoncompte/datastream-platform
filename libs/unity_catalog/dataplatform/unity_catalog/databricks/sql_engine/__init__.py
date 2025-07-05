0d6fc2be1036812ec1bc2c08a9e50d59e037c47f
jcompte <jcompte@inspirato.com>
2025-07-05 17:17:30 -0500

    sqlengine

---
+++ libs/unity_catalog/dataplatform/unity_catalog/databricks/sql_engine/__init__.py
@@ -0,0 +1,3 @@
+from .engine import get_table_model, sql_session
+
+__all__ = ["get_table_model", "sql_session"]
---
+++ packages/datastream/dataplatform/datastream/test_uc_library.py
@@ -0,0 +1,4 @@
+import polars as pl
+
+if __name__ == "__main__":
+    df = pl.read_csv("../../../../fixtures/DigiDB_digimonlist.csv")
